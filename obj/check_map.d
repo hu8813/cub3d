6fc228ebb5c6637d18b2c13ad1df08136d7eb7a6
hu8813 <h0553559@gmail.com>
2023-09-23 20:24:44 +0200

    a

---
+++ obj/check_map.d
@@ -0,0 +1,1 @@
+obj/check_map.o: src/check_map.c src/../inc/cub3D.h
--- src/init_map.c
+++ src/init_map.c
@@ -29,52 +29,62 @@ static void	map_split2(t_main *main, int height, int width, char *buffer)
 	}
 }
 
-static int pre_check(char *buffer)
+static int	pre_check(char *buffer)
 {
-	if (!ft_strnstr(buffer, "NO ", ft_strlen(buffer)) || !ft_strnstr(buffer, "SO ", ft_strlen(buffer))
-		|| !ft_strnstr(buffer, "WE ", ft_strlen(buffer)) || !ft_strnstr(buffer, "EA ", ft_strlen(buffer))	
-		|| !ft_strnstr(buffer, "C ", ft_strlen(buffer)) || !ft_strnstr(buffer, "F ", ft_strlen(buffer)))
+	if (!ft_strnstr(buffer, "NO ", ft_strlen(buffer)) || !ft_strnstr(buffer,
+			"SO ", ft_strlen(buffer)) || !ft_strnstr(buffer, "WE ",
+			ft_strlen(buffer)) || !ft_strnstr(buffer, "EA ", ft_strlen(buffer))
+		|| !ft_strnstr(buffer, "C ", ft_strlen(buffer)) || !ft_strnstr(buffer,
+			"F ", ft_strlen(buffer)))
 		return (0);
 	return (1);
 }
 
-static void get_color(char *buffer, int rgb[3], int *k)
+static void	get_color(char *buffer, int rgb[3], int *k)
 {
-    int i = 0;
-    int j = 0;
-    int component = 0;
-	int pos = ft_strchr_idx(&buffer[i], '\n');
+	int		i;
+	int		j;
+	int		component;
+	int		pos;
+	char	*tmp;
 
+	i = 0;
+	j = 0;
+	component = 0;
+	pos = ft_strchr_idx(&buffer[i], '\n');
 	k += pos;
 	if (pos == -1 || pos == 0 || buffer[i] == '\n')
-		return ;	
-    char *tmp = malloc(pos + 1);
-    if (tmp == NULL) {
-       ft_error("Error\nMalloc failed", NULL);
-    }
-
-    while (buffer[i]) {
-        if (buffer[i] == ',' || buffer[i] == '\n') {
-            tmp[j] = '\0';
-            rgb[component] = atoi(tmp);
+		return ;
+	tmp = malloc(pos + 1);
+	if (tmp == NULL)
+	{
+		ft_error("Error\nMalloc failed", NULL);
+	}
+	while (buffer[i])
+	{
+		if (buffer[i] == ',' || buffer[i] == '\n')
+		{
+			tmp[j] = '\0';
+			rgb[component] = atoi(tmp);
 			memset(tmp, 0, 10);
-            j = 0;
-            component++;
-
-            if (component == 3) {
-                free(tmp);
-                return ;
-            }
-        } else if (buffer[i] != ' ' && buffer[i] != '\t') {
-            tmp[j] = buffer[i];
-            j++;
-        }
-
-        i++;
-    }
-    free(tmp);
-	(void) pos;
-	(void) k;
+			j = 0;
+			component++;
+			if (component == 3)
+			{
+				free(tmp);
+				return ;
+			}
+		}
+		else if (buffer[i] != ' ' && buffer[i] != '\t')
+		{
+			tmp[j] = buffer[i];
+			j++;
+		}
+		i++;
+	}
+	free(tmp);
+	(void)pos;
+	(void)k;
 }
 
 static char	*get_texture(char *buffer, t_main *main, int *pos)
@@ -85,20 +95,22 @@ static char	*get_texture(char *buffer, t_main *main, int *pos)
 
 	i = 0;
 	k = ft_strchr_idx(&buffer[i], '\n');
-	while (buffer[i] && i < k && buffer[i] != '\n'  && (buffer[i] == ' ' || buffer[i] == '\t'))
+	while (buffer[i] && i < k && buffer[i] != '\n' && (buffer[i] == ' '
+			|| buffer[i] == '\t'))
 		i++;
-	pos+= k;
+	pos += k;
 	if (k == -1 || k == 0 || buffer[i] == '\n')
 		return (NULL);
 	k--;
-	while (buffer[k] && buffer[k] != '\n' && (buffer[k] == ' ' || buffer[k] == '\t'))
+	while (buffer[k] && buffer[k] != '\n' && (buffer[k] == ' '
+			|| buffer[k] == '\t'))
 		k--;
 	tmp = ft_substr(buffer, i, k - i + 1);
 	if (access(tmp, F_OK | R_OK) == -1)
-		{
-			free(tmp);
-			return (NULL);
-		}
+	{
+		free(tmp);
+		return (NULL);
+	}
 	else
 		return (tmp);
 	(void)main;
@@ -110,7 +122,7 @@ static void	map_split(char *buffer, t_main *main)
 	int	height;
 	int	width;
 	int	i;
-	int err;
+	int	err;
 
 	main->exitcount = 0;
 	main->playercount = 0;
@@ -120,26 +132,32 @@ static void	map_split(char *buffer, t_main *main)
 	err = 0;
 	if (!pre_check(buffer))
 		ft_error("Error\nMissing texture or color", main);
-	
 	while (buffer[i] != 0)
-		{
-			if (!ft_strncmp(&buffer[i], "\0NO ", 4) || !ft_strncmp(&buffer[i], "\nNO ", 4))
-				main->north = get_texture(&buffer[i + 4], main, &i);
-			else if (!ft_strncmp(&buffer[i], "\0SO ", 4) || !ft_strncmp(&buffer[i], "\nSO ", 4))
-				main->south = get_texture(&buffer[i + 4], main, &i);
-			else if (!ft_strncmp(&buffer[i], "\0WE ", 4) || !ft_strncmp(&buffer[i], "\nWE ", 4))
-				main->west = get_texture(&buffer[i + 4], main, &i);
-			else if (!ft_strncmp(&buffer[i], "\0EA ", 4) || !ft_strncmp(&buffer[i], "\nEA ", 4))
-				main->east = get_texture(&buffer[i + 4], main, &i);
-			else if (!ft_strncmp(&buffer[i], "\0F ", 3) || !ft_strncmp(&buffer[i], "\nF ", 3))
-				get_color(&buffer[i + 3], main->floor, &i);
-			else if (!ft_strncmp(&buffer[i], "\0C ", 3) || !ft_strncmp(&buffer[i], "\nC ", 3))
-				get_color(&buffer[i + 3], main->ceil, &i);
-			
-			i++;
-			height++;
-		}
-	if (main->floor[0] == -1 || main->ceil[0] == -1 || main->floor[1] == -1 || main->ceil[1] == -1 || main->floor[2] == -1 || main->ceil[2] == -1 || !main->north || !main->south || !main->west || !main->east)
+	{
+		if (!ft_strncmp(&buffer[i], "\0NO ", 4) || !ft_strncmp(&buffer[i],
+				"\nNO ", 4))
+			main->north = get_texture(&buffer[i + 4], main, &i);
+		else if (!ft_strncmp(&buffer[i], "\0SO ", 4) || !ft_strncmp(&buffer[i],
+					"\nSO ", 4))
+			main->south = get_texture(&buffer[i + 4], main, &i);
+		else if (!ft_strncmp(&buffer[i], "\0WE ", 4) || !ft_strncmp(&buffer[i],
+					"\nWE ", 4))
+			main->west = get_texture(&buffer[i + 4], main, &i);
+		else if (!ft_strncmp(&buffer[i], "\0EA ", 4) || !ft_strncmp(&buffer[i],
+					"\nEA ", 4))
+			main->east = get_texture(&buffer[i + 4], main, &i);
+		else if (!ft_strncmp(&buffer[i], "\0F ", 3) || !ft_strncmp(&buffer[i],
+					"\nF ", 3))
+			get_color(&buffer[i + 3], main->floor, &i);
+		else if (!ft_strncmp(&buffer[i], "\0C ", 3) || !ft_strncmp(&buffer[i],
+					"\nC ", 3))
+			get_color(&buffer[i + 3], main->ceil, &i);
+		i++;
+		height++;
+	}
+	if (main->floor[0] == -1 || main->ceil[0] == -1 || main->floor[1] == -1
+		|| main->ceil[1] == -1 || main->floor[2] == -1 || main->ceil[2] == -1
+		|| !main->north || !main->south || !main->west || !main->east)
 		ft_error("Error\nMissing texture or color", main);
 	if (buffer[i - 1] != '\n')
 		height++;
@@ -169,7 +187,7 @@ char	*get_next_line(int fd)
 	if (copy > string)
 	{
 		*copy = 0;
-		new = (char *)malloc(sizeof * new * (ft_strlen(string) + 1));
+		new = (char *)malloc(sizeof *new *(ft_strlen(string) + 1));
 		i = -1;
 		while (string[++i])
 			new[i] = string[i];
